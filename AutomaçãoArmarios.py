4bb1e2c201949f6f6a21424d2396f0a16438dd58
Rafael Coronetti <104257743+Rcoronetti@users.noreply.github.com>
2022-07-14 19:44:32 -0300

    Update AutomaçãoArmarios.py

--- AutomaçãoArmarios.py
+++ AutomaçãoArmarios.py
@@ -1,18 +1,3 @@
-# (2.5) A Unoesc pensou em adotar armários para os alunos deixarem seus materiais, da mesma forma que as universidades Americanas. Com isso, ela pensou nos alunos de Engenharia
-# de Computação para montar a automação deste sistema. Para testar o sistema terá que controlar 10 armários. Monte um menu de opções que será exibido num monitor touch de
-# controle conforme o exemplo abaixo e implemente as rotinas para que cada opção do menu funcione corretamente:
-
-# MENU
-# 1 – (0,25) Mostrar a situação de todos os armários, exemplo: Armario 0: Ocupado, Armario 1: Ocupado, Armario 2: Livre...
-# 2 – (0,25) Mostrar os armários livres, exemplo: Armario 2, Armario 4, Armario 8...
-# 3 – (0,75) Utilizar Armário: Informar o número de um armário livre e marcá-lo como ocupado, caso o armário estiver ocupado avisar o usuário ARMARIO SENDO UTILIZADO.
-# 4 – (0,75) Remover Armário: Informar o número de um armário ocupado e fazer a liberação do armário, caso o armário estiver livre avisar o usuário ARMARIO NAO ESTA SENDO UTILIZADO.
-# 5 – (0,25) Resumo dos Armários, exemplo: 3 Armários livres, 7 Armários ocupados
-# 0 - Sair
-
-# Observações: (0,25) O programa deve iniciar com todos os Armários livres e o menu deve ficar em loop infinito, ou seja, depois de escolher qualquer opção deve sempre voltar 
-# ao menu. O menu deve tratar opções inválidas.
-
 
 import time
 
